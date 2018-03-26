17bb2c8f05b4aa5463f496e93c862cf8493e8d70
Hariprasad <hariprasadcsmails@gmail.com>
2018-03-26 19:53:13 +0530

    errors update

---
+++ Errors/Stack Tracke.go
@@ -0,0 +1,26 @@
+package Errors
+
+import (
+	"fmt"
+	"log"
+	"github.com/pkg/errors"
+)
+
+// printErrStack prints the needed stack trace for the errors created via db operations.
+func printErrStack(err error) error {
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
+	}
+
+	err = errors.Wrap(err,"Error")
+	log.Print(err)
+	newErr, ok := err.(stackTracer)
+	if !ok {
+		panic("oops, err does not implement stackTracer")
+	}
+
+	st := newErr.StackTrace()
+	fmt.Printf("%+v\n", st[1:3]) // Required four frames.
+
+	return nil
+}
